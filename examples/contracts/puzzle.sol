69ab25dcae31b7184e58f3996223e5d4b325d580
Chris C <5767854+ca-carr@users.noreply.github.com>
2022-10-26 23:10:30 +0200

    Update puzzle.sol

--- examples/contracts/puzzle.sol
+++ examples/contracts/puzzle.sol
@@ -10,15 +10,16 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Storage {
 
     uint256 number;
-    bytes32 SOLUTION = 0xdc8051ffd210f581baa3117339b87469780fe32f1cbf08a1cccc7cc3676c1e11;
-    bytes32 NOG = 0x008051ffd210f581baa3117339b87469780fe32f1cbf08a1cccc7cc3676c1e11;
+    bytes32 SOLUTION = 0xf334c379ee6e505418b35854e9e9ecbb303f32bf28440452255b6f3ffc6471ce;
+    bytes32 SLOG = 0x0000000000000000000000000000000000000000000000000000000000000000;
 
     /**
      * @dev Store value in variable
      * @param num value to store
      */
-    function store(uint256 num) public {
+    function store(uint256 num) public returns(uint256) {
         number = num;
+        return (number);
     }
 
     /**
@@ -26,16 +27,33 @@ contract Storage {
      * @return value of 'number'
      */
     function retrieve() public view returns (uint256){
-        return (number * 10);
+        return (number);
 
     }
+
+
+    function display(string memory _string) public pure returns(string memory) {
+        return (_string);
+    }
+
     function hash(string memory _string) public view returns(bytes32){ 
 
         if    (keccak256(abi.encodePacked(_string)) == SOLUTION) {
-            return (NOG);
+            return (SLOG);
         }
         return keccak256(abi.encodePacked(_string));
         
+    }
+
+    function check_puzzle(string memory _string) public view returns(string memory){ 
+
+        // THIS IS THE PUZZLE FUNCTION!
+        if (hash(_string) == SLOG) {
+            return("puzzle solved");
+        } else {
+            return("wrong!");
+        }
+
     }
 
 
